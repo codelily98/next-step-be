88ec15a365e2511cf591ece8b3f572c6300f1f1f
CodeLily <codelily98@naver.com>
2025-05-20 04:13:55 +0900

    next-step-be Project First Upload

---
+++ src/test/java/com/next_step_be/next_step_be/NextStepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next_step_be.next_step_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
