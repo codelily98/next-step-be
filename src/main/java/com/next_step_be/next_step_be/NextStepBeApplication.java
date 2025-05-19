a21a50f4aea50abd971462cda8e1aabd8a26a453
CodeLily <codelily98@naver.com>
2025-05-20 04:13:55 +0900

    next-step-be Project First Upload

---
+++ src/main/java/com/next_step_be/next_step_be/NextStepBeApplication.java
@@ -0,0 +1,13 @@
+package com.next_step_be.next_step_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextStepBeApplication.class, args);
+	}
+
+}
