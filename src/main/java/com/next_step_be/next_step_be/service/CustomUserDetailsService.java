2492153c6e5a8a3ac65668b490ee35008f18d978
CodeLily <codelily98@naver.com>
2025-06-12 03:49:20 +0900

    Login Function Log Update

--- src/main/java/com/next_step_be/next_step_be/service/CustomUserDetailsService.java
+++ src/main/java/com/next_step_be/next_step_be/service/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package com.next_step_be.next_step_be.service;
 
 import com.next_step_be.next_step_be.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service // 스프링 빈으로 등록
+@Slf4j
 @RequiredArgsConstructor // final 필드를 위한 생성자 자동 생성
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -15,8 +18,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    	log.info("🔍 사용자 로드 요청: {}", username);
         // 사용자 이름으로 User 엔티티를 찾아 반환
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        		.orElseThrow(() -> {
+                    log.warn("❌ 사용자 없음: {}", username);
+                    return new UsernameNotFoundException("User not found with username: " + username);
+                });
     }
 }
